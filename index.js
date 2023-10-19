ee560af133f72aff9594c2f4772e59dba78917bd
Jerry Tarus <jerrytarus@gmail.com>
2023-10-19 17:19:00 +0300

    Created two functions to handle employee info

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+// Creating a function
+function createEmployeeRecord(employeeInfo) {
+    let employeeRecord = {};
+    employeeRecord.firstName = employeeInfo[0];
+    employeeRecord.familyName = employeeInfo[1];
+    employeeRecord.title = employeeInfo[2];
+    employeeRecord.payPerHour = employeeInfo[3];
+    employeeRecord.timeInEvents = [];
+    employeeRecord.timeOutEvents = [];
+    return employeeRecord;
+  }
+
+//   Another function
+function createEmployeeRecords(rows) {
+    return rows.map(row => createEmployeeRecord(row));
+  }
+
+  
+
+
